698cbf1aef29c43c4b2213373cc947e0f97e7115
Ross Hadden <rosshadden@gmail.com>
2018-01-15 23:50:08 -0500

    Pseudo lerp the rotation.

--- src/entities/actors/player.nim
+++ src/entities/actors/player.nim
@@ -1,8 +1,9 @@
-import strutils
+import math, strutils
 
 include ../../global
 
 import
+  ../../utils/math_utils,
   input,
   node_2d,
   polygon_2d
@@ -14,8 +15,11 @@ const
   DOWN = vec2(0, 1)
 
 gdobj Player of Polygon2d:
-  var speed: float = 200
-  var deadzone: float = 0.2
+  var speed* = 200.0
+  var angularSpeed* = 5.0
+  var deadzone* = [ 0.3, 0.5 ]
+
+  var targetRot: float
 
   method ready() =
     discard
@@ -28,13 +32,23 @@ gdobj Player of Polygon2d:
       movement = vec2(input.getJoyAxis(0, 0), input.getJoyAxis(0, 1))
 
     # keyboard
-    if movement.length < deadzone:
+    if movement.length < deadzone[0]:
       if input.isActionPressed("left"): movement += LEFT
       elif input.isActionPressed("right"): movement += RIGHT
       if input.isActionPressed("up"): movement += UP
       elif input.isActionPressed("down"): movement += DOWN
 
-    if movement.length >= deadzone: move(movement, dt)
+    if movement.length >= deadzone[0]: move(movement, dt)
+
+    # rotation
+    # TODO: use a real lerp
+    # TODO: fix crossing the +-0 angle
+    var rotation = vec2(input.getJoyAxis(0, 2), input.getJoyAxis(0, 3))
+    if rotation.length >= deadzone[1]:
+      targetRot = rotation.angle
+    if targetRot != self.rotation:
+      if abs(targetRot - self.rotation) <= 0.1: self.rotation = targetRot
+      else: self.rotation = lerp(self.rotation, targetRot, angularSpeed * dt)
 
   method move(vector: Vector2, dt: float64) {.base.} =
     var movement = vector.clamped(1) * speed * dt
---
+++ src/utils/math_utils.nim
@@ -0,0 +1,1 @@
+proc lerp*(a, b, t: float): float = (1 - t) * a + t * b
