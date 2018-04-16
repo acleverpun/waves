a799910b102154863224f6cf55a9e52a13cabdcf
Ross Hadden <rosshadden@gmail.com>
2018-04-16 17:04:13 -0400

    Added model, and rotated it where relevant.

--- src/entities/actors/player.nim
+++ src/entities/actors/player.nim
@@ -2,11 +2,25 @@ include globals
 
 import
   input,
-  kinematic_body_2d
+  kinematic_body_2d,
+  math,
+  spatial,
+  sprite,
+  viewport
 
 gdobj Player of KinematicBody2d:
   var speed* = 10000.0
   var runModifier* = 4.0
+  var viewport: Viewport
+  var sprite: Sprite
+  var model: Spatial
+
+  method ready() =
+    viewport = getNode("viewport") as Viewport
+    sprite = getNode("sprite") as Sprite
+    model = viewport.getNode("model") as Spatial
+
+    sprite.texture = viewport.getTexture()
 
   method physicsProcess(dt: float) =
     var move: Vector2
@@ -26,5 +40,5 @@ gdobj Player of KinematicBody2d:
       currentSpeed *= runModifier
 
     if move != ZERO:
-      self.rotation = move.angle
+      model.rotation = vec3(0, move.angle - PI/2, 0)
       discard moveAndSlide(move.normalized * currentSpeed * dt)
