00d4877bee618092d5bc42064d8e107431bc6e2c
Ross Hadden <rosshadden@gmail.com>
2018-04-16 20:00:45 -0400

    Got animations working.

--- src/entities/actors/player.nim
+++ src/entities/actors/player.nim
@@ -1,6 +1,8 @@
 include globals
 
 import
+  animation,
+  animation_player,
   input,
   kinematic_body_2d,
   math,
@@ -11,20 +13,30 @@ import
 gdobj Player of KinematicBody2d:
   var speed* = 10000.0
   var runModifier* = 4.0
+
+  var animation: string
+  var tween = 0.2
+
   var viewport: Viewport
   var sprite: Sprite
   var model: Spatial
+  var animPlayer: AnimationPlayer
 
   method ready() =
     viewport = getNode("viewport") as Viewport
     sprite = getNode("sprite") as Sprite
     model = viewport.getNode("model") as Spatial
+    animPlayer = model.getNode("animationPlayer") as AnimationPlayer
 
     sprite.texture = viewport.getTexture()
+
+    animPlayer.getAnimation("Idle").loop = true
+    animPlayer.getAnimation("Walk").loop = true
 
   method physicsProcess(dt: float) =
     var move: Vector2
     var currentSpeed = speed
+    var currentAnimation = animation
 
     # cardinals
     if input.isActionPressed("move_up"):
@@ -39,6 +51,13 @@ gdobj Player of KinematicBody2d:
     if input.isActionPressed("move_run"):
       currentSpeed *= runModifier
 
-    if move != ZERO:
+    if move == ZERO:
+      currentAnimation = "Idle"
+    else:
+      currentAnimation = "Walk"
       model.rotation = vec3(0, move.angle - PI/2, 0)
       discard moveAndSlide(move.normalized * currentSpeed * dt)
+
+    if currentAnimation != animation:
+      animation = currentAnimation
+      animPlayer.play(currentAnimation, tween)
