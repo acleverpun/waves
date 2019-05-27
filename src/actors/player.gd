82ce96c3beaea097c77aa3a1a3440902a010bb2b
Ross Hadden <rosshadden@gmail.com>
2019-05-27 12:25:31 -0400

    Added idle animation.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -2,6 +2,12 @@ extends KinematicBody2D
 
 onready var viewport = get_node("viewport")
 onready var sprite = get_node("sprite")
+onready var model = viewport.get_node("model")
+onready var animPlayer = model.get_node("animationPlayer")
 
 func _ready():
 	sprite.texture = viewport.get_texture()
+
+	animPlayer.get_animation("Idle").loop = true
+	animPlayer.get_animation("Walk").loop = true
+	animPlayer.play("Idle")
