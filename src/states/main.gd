307efac63bceace45f10afc83960f0fb4de58cdd
Ross Hadden <rosshadden@gmail.com>
2019-07-21 14:38:57 -0400

    Supported restarting current scene.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/states/main.gd
+++ src/states/main.gd
@@ -3,9 +3,8 @@ extends Node
 onready var Zone = preload("res://src/zones/courtyard.tscn")
 
 func _ready():
-	var zone = Zone.instance()
-
-	add_child(zone)
+	add_child(Zone.instance())
 
 func _input(event):
 	if Input.is_action_pressed("game.close"): get_tree().quit()
+	if Input.is_action_pressed("game.restart"): get_tree().reload_current_scene()
