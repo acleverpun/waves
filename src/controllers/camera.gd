5bcf9a3bdf2b6150cfc4ddfc8ce3b6c01f0d4115
Ross Hadden <rosshadden@gmail.com>
2019-07-31 01:47:28 -0400

    Cam only works when on the root of the player for some reason.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/controllers/camera.gd
+++ src/controllers/camera.gd
@@ -4,24 +4,21 @@ const zoomMax = Vector2(1.0, 1.0)
 const zoomMin = Vector2(0.1, 0.1)
 const zoomStep = Vector2(0.1, 0.1)
 
-onready var host = get_parent().get_parent()
-onready var cam = host.find_node("cam")
-
 func _ready():
 	zoom()
 
 func _input(event):
-	if Input.is_action_pressed("cam.zoom-in") and cam.zoom - zoomStep > zoomMin:
+	if Input.is_action_pressed("cam.zoom-in") and self.zoom - zoomStep > zoomMin:
 		zoom(-1)
-	elif Input.is_action_pressed("cam.zoom-out") and cam.zoom < zoomMax:
+	elif Input.is_action_pressed("cam.zoom-out") and self.zoom < zoomMax:
 		zoom(1)
 
 func zoom(level: int = 0):
-	cam.zoom += level * zoomStep
+	self.zoom += level * zoomStep
 
 	# TODO: vet need
-	# var dragMargin = 0.5 * cam.zoom.x
-	# cam.drag_margin_top = dragMargin
-	# cam.drag_margin_bottom = dragMargin
-	# cam.drag_margin_left = dragMargin
-	# cam.drag_margin_right = dragMargin
+	# var dragMargin = 0.5 * self.zoom.x
+	# self.drag_margin_top = dragMargin
+	# self.drag_margin_bottom = dragMargin
+	# self.drag_margin_left = dragMargin
+	# self.drag_margin_right = dragMargin
